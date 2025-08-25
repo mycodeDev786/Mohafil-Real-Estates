3b8bf00f2aae4d6e866d3a78e279c6627331fd23
Azhar Khan <mycodedeve@gmail.com>
2025-08-25 16:41:56 +0500

    Update layout.js

--- app/layout.js
+++ app/layout.js
@@ -24,7 +24,7 @@ export default function RootLayout({ children }) {
     <html lang="en">
       <head>
         <link
-          href="https://fonts.googleapis.com/css2?family=Inter:wght@400;600;700&display=swap"
+          href="https://fonts.googleapis.com/css2?family=Poppins:wght@300;400;500;600;700&display=swap"
           rel="stylesheet"
         />
         <link
